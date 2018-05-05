3b8a1cf43a3829d5e35d19e9435f4f1c2c21f5bf
Jess Giampietro <giampietrojess@gmail.com>
2018-05-04 23:28:26 -0400

    add bamazon manager.js

--- BamazonManager.js
+++ BamazonManager.js
@@ -4,3 +4,185 @@ const Table = require('cli-table'),
     inquirer = require('inquirer'),
     colors = require('colors');
 
+    //establish connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "VMGJ7B!X",
+    database: "Bamazon"
+})    
+
+    function start(){
+        inquirer.prompt([{
+          type: "list",
+          name: "doThing",
+          message: "What would you like to do?",
+          choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product","End Session"]
+        }]).then(function(ans){
+           switch(ans.doThing){
+            case "View Products for Sale": viewProducts();
+            break;
+            case "View Low Inventory": viewLowInventory();
+            break;
+            case "Add to Inventory": addToInventory();
+            break;
+            case "Add New Product": addNewProduct();
+            break;
+            case "End Session": console.log('Bye!');
+          }
+        });
+      }
+      
+      //views all inventory
+      function viewProducts(){
+        console.log('╔═══════════════════════════════════════════════════════════════════════════════════════════════════╗'.blue)
+        console.log('                                         Viewing Products                                           '.green)
+        console.log('╚═══════════════════════════════════════════════════════════════════════════════════════════════════╝'.blue)
+        
+      
+        connection.query('SELECT * FROM Inventory', function(err, res){
+        if(err) throw err;
+        console.log('___________________________________________________________________________________________________'.magenta)
+        console.log(' ')
+
+        for(var i = 0; i<res.length;i++){
+          console.log("ID: ".green + res[i].ItemID + " | " + "Product: ".green + res[i].ItemName + " | " + "Department: ".green + res[i].DepartmentName + " | " + "Price: ".green + res[i].Price + " | " + "QTY: ".green + res[i].Stock);
+          console.log('___________________________________________________________________________________________________'.magenta)
+        }
+      
+        start();
+        });
+      }
+      
+      //views inventory lower than 5
+      function viewLowInventory(){
+        console.log('╔═══════════════════════════════════════════════════════════════════════════════════════════════════╗'.blue)
+        console.log('                                       Viewing Low Inventory                                        '.green)
+        console.log('╚═══════════════════════════════════════════════════════════════════════════════════════════════════╝'.blue)
+        
+      
+        connection.query('SELECT * FROM Inventory', function(err, res){
+        if(err) throw err;
+        console.log('___________________________________________________________________________________________________'.magenta)
+        console.log(' ')
+
+        for(var i = 0; i<res.length;i++){
+          if(res[i].Stock <= 5){
+          console.log("ID: " + res[i].ItemID + " | " + "Product: " + res[i].ItemName + " | " + "Department: " + res[i].DepartmentName + " | " + "Price: " + res[i].Price + " | " + "QTY: " + res[i].Stock);
+          console.log('___________________________________________________________________________________________________'.magenta)
+          }
+        }
+      
+        start();
+        });
+      }
+      
+      //displays prompt to add more of an item to the store and asks how much
+      function addToInventory(){
+        console.log('╔═══════════════════════════════════════════════════════════════════════════════════════════════════╗'.blue)
+        console.log('                                       Adding to Inventory                                        '.green)
+        console.log('╚═══════════════════════════════════════════════════════════════════════════════════════════════════╝'.blue)
+        
+      
+        connection.query('SELECT * FROM Inventory', function(err, res){
+        if(err) throw err;
+        var itemArray = [];
+        //pushes each item into an itemArray
+        for(var i=0; i<res.length; i++){
+          itemArray.push(res[i].ProductName);
+        }
+      
+        inquirer.prompt([{
+          type: "list",
+          name: "product",
+          choices: itemArray,
+          message: "Which item would you like to add inventory?"
+        }, {
+          type: "input",
+          name: "qty",
+          message: "How much would you like to add?",
+          validate: function(value){
+            if(isNaN(value) === false){return true;}
+            else{return false;}
+          }
+          }]).then(function(ans){
+            var currentQty;
+            for(var i=0; i<res.length; i++){
+              if(res[i].ProductName === ans.product){
+                currentQty = res[i].Stock;
+              }
+            }
+            connection.query('UPDATE Inventory SET ? WHERE ?', [
+              {Stock: currentQty + parseInt(ans.qty)},
+              {ProductName: ans.product}
+              ], function(err, res){
+                if(err) throw err;
+                console.log('The quantity was updated.');
+                start();
+              });
+            })
+        });
+      }
+      
+      //allows manager to add a completely new product to store
+      function addNewProduct(){
+        console.log('╔═══════════════════════════════════════════════════════════════════════════════════════════════════╗'.blue)
+        console.log('                                         Adding New Product                                          '.green)
+        console.log('╚═══════════════════════════════════════════════════════════════════════════════════════════════════╝'.blue)
+        
+      
+        var deptNames = [];
+      
+        //grab name of departments
+        connection.query('SELECT * FROM Departments', function(err, res){
+          if(err) throw err;
+          for(var i = 0; i<res.length; i++){
+            deptNames.push(res[i].DepartmentName);
+          }
+        })
+      
+        inquirer.prompt([{
+          type: "input",
+          name: "product",
+          message: "Product: ",
+          validate: function(value){
+            if(value){return true;}
+            else{return false;}
+          }
+        }, {
+          type: "list",
+          name: "department",
+          message: "Department: ",
+          choices: deptNames
+        }, {
+          type: "input",
+          name: "price",
+          message: "Price: ",
+          validate: function(value){
+            if(isNaN(value) === false){return true;}
+            else{return false;}
+          }
+        }, {
+          type: "input",
+          name: "quantity",
+          message: "Quantity: ",
+          validate: function(value){
+            if(isNaN(value) == false){return true;}
+            else{return false;}
+          }
+        }]).then(function(ans){
+          connection.query('INSERT INTO Inventory SET ?',{
+            ProductName: ans.product,
+            DepartmentName: ans.department,
+            Price: ans.price,
+            Stock: ans.quantity
+          }, function(err, res){
+            if(err) throw err;
+            console.log('Another item was added to the store.');
+          })
+          start();
+        });
+      }
+      
+      start();
--- BamazonScheme.sql
+++ BamazonScheme.sql
@@ -15,8 +15,22 @@ CREATE TABLE Inventory(
 
 select * from Inventory;
 
-INSERT INTO Inventory(ItemName,DepartmentName,Price,StockQuantity)
-VALUES ("Item Name","Department Name",99.99,10);
+INSERT INTO Inventory(ItemName,DepartmentName,Price,Stock)
+VALUES ("Bioshock Infinite","Video Games",59.99,100),
+("Bananas","Food",00.50,20),
+("Leather Recliner","Furniture",159.99,50),
+("Settlers of Catan","Board Games",39.99,3),
+("Melting Clock","Decoration",29.99,37),
+("Avengers: Infinity War","Movies",999.99,1000),
+("Pikachu Piggy Bank","Miscellaneous",10.00,10),
+("Hawkeye: My Life As A Weapon","Comics",9.95,100),
+("Moriar Tea Mug","Homegoods",13.99,2),
+("Han Solo Jacket","Clothing",519.99,2),
+("God of War","Video Games",69.99,100),
+("Harry Potter and the Prisoner of Azkaban","Books",59.99,100),
+("Infinity Gauntlet","Clothing",1990909000.99,1),
+("Groot","Gardening",800.99,2),
+("Pumpkin Spice Candle","Homegoods",5.99,1000);
 
 CREATE TABLE Departments(
     DepartmentID INT AUTO_INCREMENT NOT NULL,
@@ -26,4 +40,12 @@ CREATE TABLE Departments(
     PRIMARY KEY(DepartmentID));
 
 INSERT INTO Departments(DepartmentName, OverHeadCosts, TotalSales)
-VALUES ('Department', 99.99, 999999.99);
+VALUES ('Furniture', 5000.00, 10000.00),
+('Books', 5000.00, 10000.00),
+('Clothing', 5000.00, 500.00),
+('Video Games', 5000.00, 1000.00),
+('Board Games', 50000.00, 90000.00),
+('Decoration', 5000.00, 90000.00),
+('Homegoods', 5000.00, 50000.00),
+('Movies', 5000.00, 5000.00),
+('Gardening', 100000.00, 10000.00);
