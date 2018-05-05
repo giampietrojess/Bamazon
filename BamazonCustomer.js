08b83b7d3621e219d8e43f152b7d7b1f5ce3454f
Jess Giampietro <giampietrojess@gmail.com>
2018-05-04 20:41:14 -0400

    require packages

---
+++ BamazonCustomer.js
@@ -0,0 +1,7 @@
+//Require Packages 
+const Table = require('cli-table'),
+    mysql = require('mysql'), 
+    inquirer = require('inquirer'),
+    colors = require('colors');
+
+    
---
+++ BamazonManager.js
@@ -0,0 +1,5 @@
+//Require Packages 
+const Table = require('cli-table'),
+    mysql = require('mysql'), 
+    inquirer = require('inquirer'),
+    colors = require('colors');
